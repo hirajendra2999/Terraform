cd59f9b0ea2427b4eb647b6f9712fe1b45524960
hirajendra2999 <rajendra.patel999@gmail.com>
2023-03-24 15:25:25 +0530

    --- Auto Git Commit ---

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
-provider "aws" {
+provider "aws" 
     region = "ap-south-1"
-}
 
-resource "aws_instance" "web" {
+
+resource "aws_instance" "web" 
    ami = "ami-0376ec8eacdf70aae"
    instance_type = "t2.micro"
    vpc_security_group_ids = ["sg-0274e4075f215241c"]
    key-name =  "ucanapply08jan18"
-   tags = {
+   tags = 
         Env = "dev”
         Name = web
     }
