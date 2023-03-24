0d98abbe4beba8232a86dde2693b684e36c61775
hirajendra2999 <rajendra.patel999@gmail.com>
2023-03-24 15:09:31 +0530

    --- Auto Git Commit ---

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "web" {
    ami = "ami-0376ec8eacdf70aae"
    instance_type = "t2.micro"
    vpc_security_group_ids = ["sg-0274e4075f215241c"]
-   key-name =  "shubham-idrsa"
+   key-name =  "ucanapply08jan18"
    tags = {
     Env = "dev”
     Name = "web”
