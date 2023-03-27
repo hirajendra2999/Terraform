aeddb7049d68e54cb95b59bb75913f42a499fa92
hirajendra2999 <rajendra.patel999@gmail.com>
2023-03-27 11:40:57 +0530

    --- Auto Git Commit ---

--- main.tf
+++ main.tf
@@ -1,9 +1,8 @@
 resource "aws_instance" "demo" {
  ami = “ami-0376ec8eacdf70aae”
  
- instance_type = “t2.micro”
 profile    = "default"
- instance_type    = 't2.micro'
+ instance_type    = "t2.micro"
   tags = {
    name = "Demo System"
  }
