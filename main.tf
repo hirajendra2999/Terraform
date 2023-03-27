e568333c8af50e9c0a1a3344e7f651fd9d7d0bbc
hirajendra2999 <rajendra.patel999@gmail.com>
2023-03-27 11:32:58 +0530

    --- Auto Git Commit ---

--- main.tf
+++ main.tf
@@ -11,3 +11,12 @@
    key-name =  "ucanapply08jan18"
 }
 }
+
+resource "aws_instance" "demo" {
+ ami = “ami-00831fc7c1e3ddc60”
+ instance_type = “t2.micro”
+
+ tags = {
+   name = "Demo System"
+ }
+}
