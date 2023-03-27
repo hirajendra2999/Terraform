8f05e1ba44a117282f30b421824b03bef0924364
hirajendra2999 <rajendra.patel999@gmail.com>
2023-03-27 11:34:14 +0530

    --- Auto Git Commit ---

--- main.tf
+++ main.tf
@@ -1,22 +1,11 @@
-   terraform 
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-  profile    = "default"
-     region     = "ap-south-1"
 
-   ami           = "ami-0376ec8eacdf70aae"
-  instance_type = "t2.micro"
-   vpc_security_group_ids = ["sg-0274e4075f215241c"]
-   key-name =  "ucanapply08jan18"
-}
-}
 
 resource "aws_instance" "demo" {
  ami = “ami-0376ec8eacdf70aae”
  instance_type = “t2.micro”
 profile    = "default"
      region     = "ap-south-1"
+     key-name =  "ucanapply08jan18"
  tags = {
    name = "Demo System"
  }
