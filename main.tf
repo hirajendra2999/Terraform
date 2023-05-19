96c44338d48679eca3d722ce2f37636551fd0359
hirajendra2999 <rajendra.patel999@gmail.com>
2023-05-19 22:13:26 +0530

    --- Auto Git Commit ---

--- main.tf
+++ main.tf
@@ -1,15 +1,3 @@
- provider “aws” {
-    region = “ap-south-1”
-}
- 
-resource “aws_instance” “my-ec2” {
-    ami = “ami-0b08bfc6ff7069aff”
-    {
-    instance_type = "t2.micro"
-    key-name = "ucanapply08jan18"
-}
-}
-
 
 provider "aws" {
     region = "ap-south-1"
