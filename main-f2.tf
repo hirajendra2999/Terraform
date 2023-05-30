03b0318f2d06b39f08ed537ee8fa7949ad9c56bb
hirajendra2999 <rajendra.patel999@gmail.com>
2023-05-30 10:18:04 +0530

    --- Auto Git Commit ---

---
+++ main-f2.tf
@@ -0,0 +1,38 @@
+ provider "aws" {
+    region = "ap-south-1"
+}
+
+resource "aws_instance"  "my-instance"{
+    ami = var.ami_ids
+    instance_type = var.instance-type
+    key_name = var.key_pair
+    tags = var.tags
+    vpc_security_group_ids = var.sg_ids
+}
+
+variable  "region" {
+    description = "please enter resion"
+    default = "ap-south-1"
+}
+variable "key_pair" {
+    default = "rajmumbai"
+}
+variable "ami_ids" {
+    default = "ami-0b08bfc6ff7069aff"
+}
+variable "instance-type"{
+    default = "t2.micro"
+}
+
+variable  "tags" {
+    type = map
+    default= {
+        env="dev"
+        Name = "my-instance"
+    }
+}
+variable  "sg_ids" {
+    type = list 
+    default = ["sg-0bcd5fa924b7d0ef0"]
+    
+}
