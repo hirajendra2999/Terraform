3f30711aa973a32e2607e96a4e8747337cdcaff0
hirajendra2999 <rajendra.patel999@gmail.com>
2023-05-30 21:43:15 +0530

    --- Auto Git Commit ---

--- main1.tf
+++ main1.tf
@@ -1,10 +1,18 @@
- provider "aws" {
+terraform{
+    bakend "s3" {
+        bucket = "terraform-buckend-r27"
+        region = "ap-south-1"
+        key = "terraform.tfstate"
+    }
+}
+
+provider "aws" {
     region = "ap-south-1"
 }
 
-resource "aws_instance"  "my-instance"{
+resource "aws_instance"  "my_instance"{
     ami = var.ami_ids
-    instance_type = var.instance-type
+    instance_type = var.instance_type
     key_name = var.key_pair
     tags = var.tags
     vpc_security_group_ids = var.sg_ids
@@ -20,7 +28,7 @@ variable "key_pair" {
 variable "ami_ids" {
     default = "ami-0b08bfc6ff7069aff"
 }
-variable "instance-type"{
+variable "instance_type"{
     default = "t2.micro"
 }
 
