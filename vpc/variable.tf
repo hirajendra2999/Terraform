3480698e51283c17085e58ed945be4af4228a16d
hirajendra2999 <rajendra.patel999@gmail.com>
2023-06-07 07:54:03 +0530

    --- Auto Git Commit ---

---
+++ vpc/variable.tf
@@ -0,0 +1,21 @@
+variable  "region" {
+    description = "please enter resion"
+    default = "ap-south-1"
+    }
+variable "instance_type"{
+    default = "t2.micro"
+}
+variable "key_pair" {
+    default = "rajmumbai"
+}
+variable "ami_ids" {
+    default = "ami-0b08bfc6ff7069aff"
+
+}
+variable  "tags" {
+    type = map
+    default= {
+        env="dev"
+        Name = "my-instance"
+    }
+}
