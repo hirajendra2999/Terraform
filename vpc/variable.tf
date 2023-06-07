7846a16a7e3a48d8908c1f5573a1b3cc8417b0b4
hirajendra2999 <rajendra.patel999@gmail.com>
2023-06-07 07:54:48 +0530

    --- Auto Git Commit ---

--- vpc/variable.tf
+++ vpc/variable.tf
@@ -1,4 +1,14 @@
+terraform{
+    backend "s3" {
+        bucket = "terraform-buckend-r271"
+        region = "ap-south-1"
+        key = "terraform.tfstate"
+    }
+}
 
+provider "aws" {
+    region = "ap-south-1"
+}
 
 
 variable  "region" {
