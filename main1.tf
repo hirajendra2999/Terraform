5d5c54db39d462993ce8f6e38921372fb1c86a15
hirajendra2999 <rajendra.patel999@gmail.com>
2023-06-07 07:19:34 +0530

    --- Auto Git Commit ---

--- main1.tf
+++ main1.tf
@@ -25,7 +25,7 @@ resource "aws_instance"  "my_instance"{
     instance_type = var.instance_type
     key_name = var.key_pair
     tags = var.tags
-    vpc_security_group_ids = [data.aws_security_group.mysg_ids]
+    vpc_security_group_ids = [data.aws_security_group.my_sg.id]
 }
 
 variable  "region" {
