def2288c1cb89edc8644acab5c5806b0e6288739
hirajendra2999 <rajendra.patel999@gmail.com>
2023-06-14 10:36:16 +0530

    --- Auto Git Commit ---

--- vpc/main.tf
+++ vpc/main.tf
@@ -11,10 +11,9 @@ provider "aws" {
 }
 
 resource "aws_vpc" "my_vpc" {
-    name = var.vpc_name
-    cidr =  var.vpc_cidr
+    cidr_block =  var.vpc_cidr
     tags = {
-        name = var.name
+        Name = "${var.project}-vpc"
         env = var.env
     }
   
@@ -23,5 +22,110 @@ resource "aws_vpc" "my_vpc" {
 resource "aws_subnet" "pri_subnet" {
     vpc_id = aws_vpc.my_vpc.id
     cidr_block = var.pri_sub_cidr
-    availability_zone  
+    availability_zone = var.az1
+    tags = {
+        Name = ${var.project}bz-private-aws_subnet
+        env = var.env
+    }  
 }
+
+resource "aws_subnet" "pub_subnet" {
+    vpc_id = aws_vpc.my_vpc.id
+    cidr_block = var.pub_sub_cidr
+    availability_zone = var.az2
+    tags = {
+        Name = "${var.project}-public-subnet"
+        env = var.env
+    }  
+    map_public_ip_on_lanch = true
+}
+resoutput "aws_internet_gateway" "my_igw" {
+    vpc_id = aws_vpc.my_vpc.id
+    tags = {
+        Name = "${var.project}-igw"
+        env = var.env
+    }
+}
+
+resoutput "aws_default_route_table" "main_rt" {
+    default_route_table_id = aws_vpc.my_vpc.default_route_table_id
+route {
+    cidr_block = "0.0.0.0"
+    gateway_id = aws_internet_gateway.my_igw.id
+    }  
+tags = {
+    Name = "${var.project}-rt"
+    env = var.env
+}
+}
+resource "aws_security_group" "sgl" {
+    name = "${var.project}.sg"
+    vpc_id = aws_vpc.my_vpc.id
+    description = "Allow http service"
+
+
+ingress {
+    protocol = "tcp"
+    from_port = 80
+    to_port = 80
+    cidr_blocks = ["0.0.0.0/0"]
+}
+
+ingress {
+    protocol = "tcp"
+    from_port = 443
+    to_port = 443
+    cidr_blocks = ["0.0.0.0/0"]
+}
+
+egress {
+    protocol = "-1"
+    from_port = 0
+    to_port = 0
+    cidr_blocks = ["0.0.0.0/0"]
+}
+depends on = [
+    aws_vpc.my_vpc
+]
+
+}
+
+resresource "aws_instance" "instance1" {
+  ami = var.image_id
+  instance_type = var.instance_type
+  vpc_security_group_ids = [aws_vpc.my_vpc.default_security_group_id, aws_security_group.sg1.id]
+subnet_id = aws_subnet.pri_subnet.id
+key_name = var.key_pair
+tags = {
+    Name = "${var.project}-private-instance"
+    env = var.env
+}
+depends on = [
+    aws_security_group.sg1
+]
+}
+
+resresource "aws_instance" "instance2" {
+  ami = var.image_id
+  instance_type = var.instance_type
+  vpc_security_group_ids = [aws_vpc.my_vpc.default_security_group_id, aws_security_group.sg1.id]
+subnet_id = aws_subnet.pub_subnet.id
+key_name = var.key_pair
+tags = {
+    Name = "${var.project}-public-instance"
+    env = var.env
+}
+user_data =<< EOT
+#!/bin/bash
+yum install httpd -y
+systemctl start httpd
+systemctl enable httpd
+echo "<h1> Hello World" > /var/www/html/index.html
+EOT
+}
+
+
+
+
+
+
--- vpc/variable.tf
+++ vpc/variable.tf
@@ -2,30 +2,52 @@
 variable  "region" {
        default = "ap-south-1"
     }
+variable "az1" {
+  default = "ap-south-1a"
+}
+variable "az2" {
+  default = "ap-south-1b"
+}
+
 variable "terraform-buckend" {
         default = "terraform-buckend-r271"
 }
-variable "vpc_name" {
-    default = "cbz-vpc"
-    }
 variable "vpc_cidr" {
     default = "10.10.0.0/16"
 }
+
 variable "env"{
     default = "dev"
 }
 
 variable "pri_sub_cidr" {
-    default = "rajmumbai"
+    default = "10.10.0.0/20"
 }
-variable "ami_ids" {
-    default = "ami-0b08bfc6ff7069aff"
 
-}
-variable  "tags" {
-    type = map
-    default= {
-        env="dev"
-        Name = "my-instance"
+variable "project" {
+    default = "cbz"
     }
+
+variable "pub_sub_cidr" {
+    default = "10.10.16.0/20"
 }
+
+variable  "image_id" {
+    default = "ami-0b08bfc6ff7069aff"
+}
+variavariable " instance_type" {
+  default = "t2.micro"
+}
+
+variable  "key_pair" {
+    default = "rj-key"
+}
+
+
+
+
+
+
+
+
+
