01b46a86a4afed544058bf607fb286b63c46fc0b
hirajendra2999 <rajendra.patel999@gmail.com>
2023-06-15 05:53:24 +0530

    --- Auto Git Commit ---

--- first_terraform_scrift.tf
+++ first_terraform_scrift.tf
@@ -15,7 +15,7 @@ resource "aws_instance"  "my_instance"{
     instance_type = var.instance_type
     key_name = var.key_pair
     tags = var.tags
-    vpc_security_group_ids = [sg-0bcd5fa924b7d0ef0"]
+    vpc_security_group_ids = ["sg-0bcd5fa924b7d0ef0"]
 }
 
 variable  "region" {
