7f7c9bc6c977f8464b2d0cce29279ba36435436b
hirajendra2999 <rajendra.patel999@gmail.com>
2023-06-15 05:58:41 +0530

    --- Auto Git Commit ---

--- first_terraform_scrift.tf
+++ first_terraform_scrift.tf
@@ -27,13 +27,15 @@ variable  "region" {
     description = "please enter resion"
     default = "ap-south-1"
     }
-    
+
 variable "instance_type"{
     default = "t2.micro"
 }
+
 variable "key_pair" {
     default = "rajmumbai"
 }
+
 variable "ami_ids" {
     default = "ami-0b08bfc6ff7069aff"
 
