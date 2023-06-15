7df06c1370e0175c80638c466ab212dd09868212
hirajendra2999 <rajendra.patel999@gmail.com>
2023-06-15 05:48:38 +0530

    --- Auto Git Commit ---

--- first_terraform_scrift.tf
+++ first_terraform_scrift.tf
@@ -1,11 +1,11 @@
-terraform {
+/*terraform {
     backend "s3" {
         bucket = "terraform-buckend-r271"
         region = "ap-south-1"
         key = "terraform.tfstate"
     }
 }
-
+*/
 
 provider "aws" {
     region = var.region
