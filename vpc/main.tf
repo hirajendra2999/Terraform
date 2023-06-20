7a0567832d04a4aa6db9e6570557a8991f3536cb
hirajendra2999 <rajendra.patel999@gmail.com>
2023-06-20 09:41:09 +0530

    --- Auto Git Commit ---

--- vpc/main.tf
+++ vpc/main.tf
@@ -49,5 +49,7 @@ resource "aws_internet_gateway" "my_igw" {
 
 resource "aws_default_route_table" "main_rt" {
   default_route_table_id = aws_vpc.my_vpc.default_route_table_id
-  route = 
+  route {
+    cidr
+  }
 }
