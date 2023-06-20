647a98881d3c7f82842e5d066ed2f05e135202b4
hirajendra2999 <rajendra.patel999@gmail.com>
2023-06-20 09:28:04 +0530

    --- Auto Git Commit ---

--- vpc/variable.tf
+++ vpc/variable.tf
@@ -42,7 +42,7 @@ variable "subnet_name" {
 defdefault = "cbz-private-subnet"  
 }
 
-/Pvariable "ami_ids" {
+/*Pvariable "ami_ids" {
     default = "ami-0b08bfc6ff7069aff"
 
 }
@@ -52,4 +52,4 @@ variable  "tags" {
         env="dev"
         Name = "my-instance"
     }
-}
+}*/
