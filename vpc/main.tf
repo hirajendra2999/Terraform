d462537614509e172bdb3cfcc7626eae12962554
hirajendra2999 <rajendra.patel999@gmail.com>
2023-06-20 10:02:01 +0530

    --- Auto Git Commit ---

--- vpc/main.tf
+++ vpc/main.tf
@@ -80,8 +80,8 @@ resource "aws_security_group" "sg1" {
 
  egress {
     protocol = "TCP"
-    from_port = 00
-    to_port = oo
+    from_port = 0
+    to_port = 0
     cidr_blocks = "0.0.0.0/0"
   }
 
