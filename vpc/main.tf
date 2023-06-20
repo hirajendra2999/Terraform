d421fa4961f06aab4320c481eeef39a77e389c53
hirajendra2999 <rajendra.patel999@gmail.com>
2023-06-20 09:08:12 +0530

    --- Auto Git Commit ---

--- vpc/main.tf
+++ vpc/main.tf
@@ -31,7 +31,7 @@ resource "aws_subnet" "pri_subnet" {
 resource "aws_subnet" "pub_subnet" {
     vpc_id = aws_vpc.my_vpc.id
     cidr_block = var.pub_sub_cidr
-    availability_zone = var.az1
+    availability_zone = var.az2
     tags = {
         Name = "${var.project}-public-subnet"
         env = var.env
