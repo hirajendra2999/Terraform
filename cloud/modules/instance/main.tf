58fb1ac86402bee1928acbe5b724c3554603ccbc
hirajendra2999 <rajendra.patel999@gmail.com>
2023-06-20 18:12:21 +0530

    --- Auto Git Commit ---

--- cloud/modules/instance/main.tf
+++ cloud/modules/instance/main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "foo" {
  ami = var.image_id
  instance_type = var.instance_type
- vpc_security_group_ids = var.sg_ids   
+ vpc_security_group_ids = var.vpc_security_group_ids   
 }
