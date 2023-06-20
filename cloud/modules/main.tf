914b42a9bb63e61f2c2779b9d8d150b14d5a276b
hirajendra2999 <rajendra.patel999@gmail.com>
2023-06-20 17:34:23 +0530

    --- Auto Git Commit ---

--- cloud/modules/main.tf
+++ cloud/modules/main.tf
@@ -9,7 +9,7 @@ cidr_block = "10.10.0.0/16"
 resource "aws_subnet" "main" {
   vpc_id = aws_vpc.my_vpc.id
   cidr_block = "10.0.1.0/24"
-  tags = {
+  tags = { 
     Name =  "${var.project}-vpc"
     env = var.env
   }
