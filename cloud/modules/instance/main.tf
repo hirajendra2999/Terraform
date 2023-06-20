b7140a89a4a3418a7aa0ed2d86cdebb662f4392a
hirajendra2999 <rajendra.patel999@gmail.com>
2023-06-20 18:15:22 +0530

    --- Auto Git Commit ---

--- cloud/modules/instance/main.tf
+++ cloud/modules/instance/main.tf
@@ -2,8 +2,8 @@ resource "aws_instance" "foo" {
  ami = var.image_id
  instance_type = var.instance_type
  vpc_security_group_ids = var.sg_ids
-key_name = var.key_pair
-tags = {
+ key_name = var.key_pair
+ tags = {
     Name = "${var.project}-instance"
     env = var.env
 }
