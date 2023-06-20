51cd421afe0d61ce7afd0e4e803724852b95ac14
hirajendra2999 <rajendra.patel999@gmail.com>
2023-06-20 08:27:05 +0530

    --- Auto Git Commit ---

---
+++ vpc/main.tf
@@ -0,0 +1,27 @@
+terraform{
+    backend "s3" {
+        bucket = "terraform-buckend-r271"
+        region = "ap-south-1"
+        key = "terraform.tfstate"
+    }
+}
+
+provider "aws" {
+    region = var.region
+}
+
+resource "aws_vpc" "my_vpc" {
+    name = var.vpc_name
+    cidr =  var.vpc_cidr
+    tags = {
+        name = var.name
+        env = var.env
+    }
+  
+}
+
+resource "aws_subnet" "pri_subnet" {
+    vpc_id = aws_vpc.my_vpc.id
+    cidr_block = var.pri_sub_cidr
+    availability_zone  
+}
---
+++ vpc/variable.tf
@@ -0,0 +1,31 @@
+
+variable  "region" {
+       default = "ap-south-1"
+    }
+variable "terraform-buckend" {
+        default = "terraform-buckend-r271"
+}
+variable "vpc_name" {
+    default = "cbz-vpc"
+    }
+variable "vpc_cidr" {
+    default = "10.10.0.0/16"
+}
+variable "env"{
+    default = "dev"
+}
+
+variable "pri_sub_cidr" {
+    default = "rajmumbai"
+}
+variable "ami_ids" {
+    default = "ami-0b08bfc6ff7069aff"
+
+}
+variable  "tags" {
+    type = map
+    default= {
+        env="dev"
+        Name = "my-instance"
+    }
+}
