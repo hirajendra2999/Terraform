906509144d9c38e32f2148bb336dbc6aacc6c4bb
hirajendra2999 <rajendra.patel999@gmail.com>
2023-06-20 10:16:20 +0530

    --- Auto Git Commit ---

--- vpc/main.tf
+++ vpc/main.tf
@@ -111,5 +111,5 @@ user_data = #!/bin/bash
 yum install httpd -y
 systemctl start httpd
 systemctl enable httpd
-echo "<h1> hello world" > /var/www/html
+echo "<h1> hello world" > /var/www/html/index.html
 }
