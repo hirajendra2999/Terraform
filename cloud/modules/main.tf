b67a05bd6462718baa1795ecd26a81bb576a33cc
hirajendra2999 <rajendra.patel999@gmail.com>
2023-06-20 17:36:54 +0530

    --- Auto Git Commit ---

--- cloud/modules/main.tf
+++ cloud/modules/main.tf
@@ -15,7 +15,7 @@ resource "aws_subnet" "pri_subnet" {
   }
 }
 
-resource "aws_subnet" "pub" {
+resource "aws_subnet" "pub_subnet" {
   vpc_id = aws_vpc.my_vpc.id
   cidr_block = "10.10.16.0/20"
   tags = { 
