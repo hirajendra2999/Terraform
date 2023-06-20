be7a8237de7edd513177025f16d007705190779b
hirajendra2999 <rajendra.patel999@gmail.com>
2023-06-20 10:09:33 +0530

    --- Auto Git Commit ---

--- vpc/main.tf
+++ vpc/main.tf
@@ -90,5 +90,5 @@ resource "aws_instance" "instance1" {
   ami = var.image_id
   instance_type = var.instance_type
   vpc_security_group_ids = [aws_vpc.my_vpc.default_security_group_id]
-
+subnet_id = aws
 }
