990d857a09c810a0105ffe3e5d276a8deaba0fe5
hirajendra2999 <rajendra.patel999@gmail.com>
2023-06-21 06:45:47 +0530

    --- Auto Git Commit ---

--- cloud/modules/vpc/variable.tf
+++ cloud/modules/vpc/variable.tf
@@ -3,3 +3,8 @@ variable "vpc_cidr" {}
 variable "env" {}
 variable "pri_sub_cidr" {}
 variable "pub_sub_cidr" {}
+variable  "project" {}
+variable "vpc_cidr" {}
+variable "env" {}
+variable "pri_sub_cidr" {}
+variable "pub_sub_cidr" {}
