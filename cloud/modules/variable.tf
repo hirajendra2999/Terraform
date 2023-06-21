d03ab54a2e20dde4f3cca821e1305255ac573a56
hirajendra2999 <rajendra.patel999@gmail.com>
2023-06-21 07:35:32 +0530

    --- Auto Git Commit ---

--- cloud/modules/variable.tf
+++ cloud/modules/variable.tf
@@ -17,3 +17,19 @@ variable "private_cidr" {
 variable "public_cidr" {
     default = "10.10.16.0/20"
 }
+
+variable "vpc_cidr" {
+    default = "10.10.0.0/16"
+}
+
+variable "env"{
+    default = "dev"
+}
+
+variable "private_cidr" {
+    default = "10.10.0.0/20"
+}
+
+variable "public_cidr" {
+    default = "10.10.16.0/20"
+}
