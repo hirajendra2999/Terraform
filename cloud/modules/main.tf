5806d9dace193992c7524ef07d1ecdca25a1ccc6
hirajendra2999 <rajendra.patel999@gmail.com>
2023-06-21 07:53:53 +0530

    --- Auto Git Commit ---

--- cloud/modules/main.tf
+++ cloud/modules/main.tf
@@ -33,7 +33,9 @@ module "my_instance" {
 
 # module "my_instance" {
 #  source = "./modules/instance"
-###  key_pair = var.key_pair
+# image_id = var.image_id
+#  instance_type = var.instance_type
+#  key_pair = var.key_pair
   project = var.project
   env = var.environment
   subnet_id = module.my_vpc_module.pub
