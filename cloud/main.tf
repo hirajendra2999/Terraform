cdcfaf926d5582f2f64084806e251cda7c7c4d00
raj <raj@gmail.com>
2023-06-23 09:08:43 +0530

    --- Auto Git Commit ---

--- cloud/main.tf
+++ cloud/main.tf
@@ -51,7 +51,7 @@ resource "aws_security_group" "my_sg" {
     
 module "my_instance" {
   source = "./modules/instance"
-  count = var.count
+  instance_count = 
   image_id = var.image_id
   instance_type = var.instance_type
   key_pair = var.key_pair
