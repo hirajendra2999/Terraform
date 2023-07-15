c860b2f98002d3fe1d77a8af41c59897c7b99c54
raj <raj@gmail.com>
2023-07-15 11:50:56 +0530

    --- Auto Git Commit ---

--- cloud/variable.tf
+++ cloud/variable.tf
@@ -1,20 +1,20 @@
 variable "project" {
-    default           = "cloudblitz"
+    default            = "cloudblitz"
     }
 variable "region" {
-    default          = "ap-south-1"
+    default           = "ap-south-1"
 }
 variable "vpc_cidr" {
-    default          = "10.10.0.0/16"
+    default           = "10.10.0.0/16"
 }
 variable "environment"{
     default           = "dev"
 }
 variable "private_cidr" {
-    default = "10.10.0.0/20"
+    default           = "10.10.0.0/20"
 }
 variable "public_cidr" {
-    default = "10.10.16.0/20"
+    default           = "10.10.16.0/20"
 }
 
 variable "instance_count" {
