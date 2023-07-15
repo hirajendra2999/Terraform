008041d3656583cc9fd1a142bd56e1179350f2b1
raj <raj@gmail.com>
2023-07-15 11:46:10 +0530

    --- Auto Git Commit ---

--- cloud/main.tf
+++ cloud/main.tf
@@ -28,13 +28,13 @@ resource "aws_security_group" "my_sg" {
     protocol           = "TCP"
     from_port          = 80
     to_port            = 80
-    cidr_blocks          = ["0.0.0.0/0"]
+    cidr_blocks        = ["0.0.0.0/0"]
   }
 
   ingress {
-    protocol = "TCP"
-    from_port = 443
-    to_port = 443
+    protocol           = "TCP"
+    from_port          = 443
+    to_port            = 443
     cidr_blocks = ["0.0.0.0/0"]
 }
 
