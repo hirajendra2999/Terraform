d2a7c4575d51e1ec667c278a17cfd2eacfafa42a
raj <raj@gmail.com>
2023-07-15 12:06:32 +0530

    --- Auto Git Commit ---

--- vpc/main.tf
+++ vpc/main.tf
@@ -1,19 +1,19 @@
 terraform{
     backend "s3" {
-        bucket       = "terraform-buckend-r271"
+        bucket         = "terraform-buckend-r271"
         region         = "ap-south-1"
         key = "terraform.tfstate"
     }
 }
 
 provider "aws" {
-    region = var.region
+    region             = var.region
 }
 
 resource "aws_vpc" "my_vpc" {
-    cidr_block = var.vpc_cidr
-    tags = {
-        Name = "${var.project}-vpc"
+    cidr_block         = var.vpc_cidr
+    tags               = {
+        Name           = "${var.project}-vpc"
         env = var.env
     }  
 }
