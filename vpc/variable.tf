3d66feede1685dcdc577bb46095c8694b30958a8
raj <raj@gmail.com>
2023-07-15 12:04:46 +0530

    --- Auto Git Commit ---

--- vpc/variable.tf
+++ vpc/variable.tf
@@ -1,23 +1,18 @@
 variable  "region" {
        default      = "ap-south-1"
     }
-
 variable "az1" {
   default           = "ap-south-1a"
 }
-
 variable "az2" {
-  default          = "ap-south-1b"
+  default         = "ap-south-1b"
 }
-
 variable "terraform-buckend" {
         default   = "terraform-buckend-r271"
 }
-
 variable "vpc_name" {
     default       = "cbz-vpc"
     }
-
 variable "vpc_cidr" {
     default      = "10.10.0.0/16"
 }
