18f3bc91e0a2dd403803f1add980a9ba47f07fde
raj <raj@gmail.com>
2023-07-15 11:47:25 +0530

    --- Auto Git Commit ---

--- cloud/main.tf
+++ cloud/main.tf
@@ -62,10 +62,10 @@ module "my_instance" {
   }
 
 # module "my_instance" {
-#  source = "./modules/instance"
-#  image_id = var.image_id
-#  instance_type = var.instance_type
-#  key_pair = var.key_pair
+#  source               = "./modules/instance"
+#  image_id             = var.image_id
+#  instance_type        = var.instance_type
+#  key_pair             = var.key_pair
 #  project = var.project
 #  env = var.environment
 #  subnet_id = module.my_vpc_module.pub
