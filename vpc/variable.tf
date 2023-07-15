fb346e834de9033e211d380e23a5c58e7277295e
raj <raj@gmail.com>
2023-07-15 12:11:49 +0530

    --- Auto Git Commit ---

--- vpc/variable.tf
+++ vpc/variable.tf
@@ -11,16 +11,16 @@ variable "terraform-buckend" {
   default         = "terraform-buckend-r271"
 }
 variable "vpc_name" {
-  default       = "cbz-vpc"
+  default         = "cbz-vpc"
     }
 variable "vpc_cidr" {
-  default      = "10.10.0.0/16"
+  default         = "10.10.0.0/16"
 }
 variable "env"{
-  default      = "dev"
+  default         = "dev"
 }
 variable "pri_sub_cidr" {
-  default      = "10.10.0.0/20"
+  default        = "10.10.0.0/20"
 }
 variable "project" {
   default        = "cbz"
