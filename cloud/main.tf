5dc7e963a8e8a04524f2beb05cec2a56a427f436
raj <raj@gmail.com>
2023-07-15 11:45:54 +0530

    --- Auto Git Commit ---

--- cloud/main.tf
+++ cloud/main.tf
@@ -25,10 +25,10 @@ resource "aws_security_group" "my_sg" {
     description       = "allow http and https service"
   
   ingress {
-    protocol = "TCP"
-    from_port = 80
-    to_port = 80
-    cidr_blocks = ["0.0.0.0/0"]
+    protocol           = "TCP"
+    from_port          = 80
+    to_port            = 80
+    cidr_blocks          = ["0.0.0.0/0"]
   }
 
   ingress {
