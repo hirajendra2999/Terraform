1df7d2351970ec6d8a361d6c632dcb6730758c05
raj <raj@gmail.com>
2023-07-15 12:12:04 +0530

    --- Auto Git Commit ---

--- vpc/variable.tf
+++ vpc/variable.tf
@@ -5,28 +5,28 @@ variable "az1" {
   default          = "ap-south-1a"
 }
 variable "az2" {
-  default         = "ap-south-1b"
+  default           = "ap-south-1b"
 }
 variable "terraform-buckend" {
-  default         = "terraform-buckend-r271"
+  default          = "terraform-buckend-r271"
 }
 variable "vpc_name" {
-  default         = "cbz-vpc"
+  default          = "cbz-vpc"
     }
 variable "vpc_cidr" {
-  default         = "10.10.0.0/16"
+  default          = "10.10.0.0/16"
 }
 variable "env"{
-  default         = "dev"
+  default          = "dev"
 }
 variable "pri_sub_cidr" {
-  default        = "10.10.0.0/20"
+  default         = "10.10.0.0/20"
 }
 variable "project" {
-  default        = "cbz"
+  default         = "cbz"
 }
 variable "pub_sub_cidr" {
-  default      = "10.10.16.0/20"
+  default         = "10.10.16.0/20"
 } 
 variable "image_id" {
   default      = "ami-0b08bfc6ff7069aff"  
