5da2440ec7eff33bf56503625a81580e00c436c0
raj <raj@gmail.com>
2023-07-15 11:24:48 +0530

    --- Auto Git Commit ---

--- cloud/modules/vpc/main.tf
+++ cloud/modules/vpc/main.tf
@@ -1,15 +1,3 @@
-/*resource "aws_vpc" "vpc" {
-  cidr_block = "${var.cidr_vpc}"
-  enable_dns_support   = true
-  enable_dns_hostnames = true
-  tags {
-    "Environment" = "${var.environment_tag}"
-  }
-}
-*/
-
-
-
 resource "aws_vpc" "my_vpc" {
 cidr_block = var.vpc_cidr
 enable_dns_support   = true
